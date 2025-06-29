70574d0107324ef6a08a0d7a3cee7270b518e181
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2025-06-29 15:27:13 +0000

    adde

--- AbnormalConditions.java
+++ AbnormalConditions.java
@@ -1,22 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class AbnormalConditions {
 public static void main(String[] args) {
 try {
 generateNumberFormatException();
 } catch (NumberFormatException e) {
 System.out.println("Caught NumberFormatException");
-e.printStackTrace();
+// e.printStackTrace();
 }
 try {
 generateStackOverflowError(1);
 } catch (StackOverflowError e) {
 System.out.println("Caught StackOverflowError");
-e.printStackTrace();
+// e.printStackTrace();
 }
 try {
 generateOutOfMemoryError();
 } catch (OutOfMemoryError e) {
 System.out.println("Caught OutOfMemoryError");
-e.printStackTrace();
+// e.printStackTrace();
+}
+try{
+    generateClassCastException();
+}
+catch(ClassCastException e){
+    System.out.println("Caught class cast exception");
+}
+try {
+    generateFileNotFoundException();
+}
+catch(FileNotFoundException e){
+    System.out.println("File not found");
 }
 }
 private static void generateNumberFormatException() {
@@ -34,5 +50,16 @@ Object[] array = new Object[1000000];
 for (int i = 0; i < array.length; i++) {
 array[i] = new byte[1024 * 1024]; 
 }
+}
+private static void generateClassCastException(){
+    Object obj = "helllo";
+    Integer i = (Integer) obj;
+}
+private static void generateFileNotFoundException()throws FileNotFoundException{
+    File file = new File("does_not_exist.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                continue;
+            }
 }
 }
